39edfbed419b1e20ce631ef14ac1cccafd0ed298
Owen Jarrett <owen.t.jarrett@gmail.com>
2020-05-26 12:36:41 -0700

    Boiler plate code for GeoJSON Flask service

---
+++ src/service/building_locations.py
@@ -0,0 +1,18 @@
+import flask
+import os
+from flask import abort, request, jsonify, Flask
+
+DATA_DIR = os.environ.get('DATA_DIR')
+TEST_JSON = 'seattle.json'
+
+app = Flask(__name__)
+reg = None
+place_info = None
+
+@app.route('/buildings', methods=['GET'])
+def get_buildings():
+    with open(f'{DATA_DIR}{os.sep}{TEST_JSON}','r') as geo_db:
+        return geo_db.readline()
+
+if __name__ == '__main__':
+    app.run()
